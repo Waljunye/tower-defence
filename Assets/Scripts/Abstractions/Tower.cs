2ae953255383bc38d1bed04245bf4f979d3aa7c5
Roma Ag <93056140+Tiraniell@users.noreply.github.com>
2022-08-22 22:51:10 +0300

    Added an abstract class for all towers and interfaces
    Added an abstract class for all towers and interfaces

---
+++ Assets/Scripts/Abstractions/Tower.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AllTower : IWatch , IRotation,IShoot, ILevelUp
+{
+    public void Coins()
+    {
+        
+    }
+
+    public void MakeDamage()
+    {
+        
+    }
+
+    public void Rotate()
+    {
+       
+    }
+
+    public void TowerUpgrade()
+    {
+
+    }
+
+    public void WatchEnemy()
+    {
+        
+    }
+}
